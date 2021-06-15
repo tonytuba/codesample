54812e73fe9e974a0cf0809c04074e3bdd7318c6
Tony Licavoli <6775563+tonytuba@users.noreply.github.com>
2021-06-14 21:19:05 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -9,7 +9,7 @@ variable "ingress_cidr" {
 variable "egress_cidr" {
     default = "0.0.0.0/0"
 }
-variable "profile" { default = "tlpersonal" }
+variable "profile" { default = "" }
 variable "region" { default = "us-east-1" }
-variable "vpc_id" { default = "vpc-0e0e9273" }
-variable "subnet_id" { default = "subnet-0e359bd36ef85ea0d" }
+variable "vpc_id" { default = "" }
+variable "subnet_id" { default = "" }
